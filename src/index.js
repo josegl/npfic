3114d705b7af01ceba6835b964e0a599372d79e5
Jose GL <jose@josegl.com>
2015-12-14 00:08:50 +0100

    code optimized. Removed comments, docs will be in the readme

--- src/index.js
+++ src/index.js
@@ -15,58 +15,36 @@ export function rAll (items, fn){
   });
 }
 
-/**
- * rES = resolveEachSecond
- * array: function fn is applied to the items
- * @fn: the function to aply to each item of the array,
- * @n: number of processes to be executed each second
- * @j: function will be applied to all items from index j, 0 by default
- */
 export function rES(array, fn, n, i = 0){
   return new Promise((resolve,reject) => {
-    if (i >= array.length)
-      return resolve();
-    else {
-      setTimeout(() => {
-        fn(array[i]).then(result => {
-          if (result)
-            console.log(result);
-          return rES(array, fn, n, i+1);
-        }).then(result => {
-          resolve(result);
-        }).catch(err =>{
-          if (err)
-            console.log(err);
-          return resolve(rES(array, fn, n, i+1));
-        });
-      },Math.floor(1000/n));
-    }
+    i >= array.length ? resolve() : setTimeout(() => {
+      fn(array[i]).then(result => {
+        if (result)
+          console.log(result);
+        return rES(array, fn, n, i+1);
+      }).then(result => {
+        resolve(result);
+      }).catch(err =>{
+        if (err)
+          console.log(err);
+        resolve(rES(array, fn, n, i+1));
+      });
+    },Math.floor(1000/n));
   });
 }
 
-/**
- * rLimit = resolve by subsets limited to n items
- * set: will be splited into subsets
- * @fn: the function to aply to each item of the array,
- * @l: number of processes to be executed each time
- * @i: function will be applied to all subsets from index i, 0 by default
- */
 export function rLimit(set, fn, l, i = 0){
   return new Promise ((resolve, reject) => {
-    if (i >= subsets(set,l).length)
+    i >= subsets(set,l).length ?
+      resolve() : rAll(subsets(set,l)[i], fn).then(()=>{
+      return rLimit(set, fn, l, i+1);
+    }).then(()=> {
       resolve();
-    else {
-      rAll(subsets(set,l)[i], fn).then(()=>{
-        return rLimit(set, fn, l, i+1);
-      }).then(()=> {
-        resolve();
-      }).catch(err => {
-        if (err)
-          console.log(err);
-        else
-          resolve(rLimit(set, fn, l, i+1));
-      });
-    }
+    }).catch(err => {
+      if(err)
+        console.log(err);
+      resolve(rLimit(set, fn, l, i+1));
+    });
   });
 }
 
