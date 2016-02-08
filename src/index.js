a4c4cdcf26f744340bdceddd81ce2ff9aebe2048
Jose Garcia Lozano <jgarloz18@gmail.com>
2016-02-08 21:56:13 +0100

    functions converted to constants and arrow anonimous functions, pure functional, removed not needed returns

--- src/index.js
+++ src/index.js
@@ -1,41 +1,32 @@
-export function rAll(fn){
-  return function (arr){
-    return new Promise((resolve, reject) => {
-      return arr.map(item => ()=> fn(item)).forEach(proc => {
-        return proc().then(res => {
-          resolve();
-        }).catch(err=>{
-          console.log(err);
-        });
+export const rAll = fn => arr => {
+  return new Promise((resolve, reject) => {
+    return arr.map(item => ()=> fn(item)).forEach(proc => {
+      return proc().then(res => {
+        resolve();
       });
     });
-  }
+  });
 }
 
-export function rES(set, fn, n){
-  return rSeq(i => {
-    return new Promise ((resolve, reject) => {
-      setTimeout(() =>{
-        resolve(fn(i));
-      },Math.floor(1000/n));
-    });
-  },set);
-}
+export const rES = (set, fn, n) => rSeq(i => {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() =>{
+      resolve(fn(i));
+    },Math.floor(1000/n));
+  });
+},set);
 
-export function rSubSeq (set, fn, l){
-  return rSeq(rAll(fn))(subsets(set,l));
-}
+export const rSubSeq = (set, fn, l) => rSeq(rAll(fn),subsets(set,l));
 
-export function rSeq (fn, set){
-  return set.map(item => ()=> fn(item)).reduce((curr, next)=> {
+export const rSeq = (fn, set) => set.map(item => ()=> fn(item)).
+  reduce((curr, next)=> {
     return curr.then(()=>{
       return  next();
     }).catch(err =>{
       return next();
     })},Promise.resolve());
-}
 
-function subsets(original, subsetSize){
+const subsets = (original, subsetSize) => {
   let a = [];
   for (let i = 0; i < original.length; i += subsetSize){
     a.push(original.slice(i, i+subsetSize));
