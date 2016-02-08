f8f68878c5ed4a5c0058b8ac5af48ea33b2a3a2e
Jose Garcia Lozano <jgarloz18@gmail.com>
2016-02-08 21:57:21 +0100

    tests updated

--- test/helpers.js
+++ test/helpers.js
@@ -7,7 +7,7 @@ function step1Error (item, t){
     }, t);
   });
 }
-function step1NoError (item, t){
+export function step1NoError (item, t){
   return new Promise((resolve, reject) => {
     setTimeout(()=>{
       resolve('step 1 for item ' + item);
@@ -15,15 +15,15 @@ function step1NoError (item, t){
   });
 }
 
-function step2 (item, t){
+export function step2 (item, t){
   return new Promise((resolve, reject) => {
     setTimeout(()=>{
       resolve('step 2 for item ' + item);
-    }, t);
+    },t);
   });
 }
 
-function step3 (item, t){
+export function step3 (item, t){
   return new Promise((resolve, reject) => {
     setTimeout(()=>{
       resolve('step 3 for item ' + item);
@@ -31,20 +31,21 @@ function step3 (item, t){
   });
 }
 
-export function processItemNoError (item) {
-  return step1NoError(item, 1000).then(result =>{
-    console.log(result);
-    return step2(item, 1000);
-  }).then(result =>{
-    console.log(result);
-    return step3(item, 1000);
-  }).then(result =>{
-    console.log(result);
-    return ('finished process of item ' + item);
-  }).catch(err =>{
-    throw (err);
-  });
+export function processItemNoError (item, time = 1000) {
+  let p = [step1NoError, step2, step3];
+  return p.reduce((promise, next)=>{
+    return promise.then(res =>{
+      if(res)
+        console.log(res);
+      return next(item,time);
+    }).then(res => {
+      console.log(res);
+    }).catch(err =>{
+      console.log(err);
+    });
+  },Promise.resolve());
 }
+
 
 export function processItemError (item) {
   return step1Error(item, 1000).then(result =>{
--- test/index.js
+++ test/index.js
@@ -56,32 +56,18 @@ describe('Promises resolve flow control', ()=>{
       let init = Date.now();
       rES(items, fastrESError, 5).then(()=>{
         let end = Date.now();
-        chai.assert.equal(Math.floor((end - init)/1000),2);
+        chai.assert.equal(Math.floor((end - init)/1000),3);
         done();
       }).catch(err => {
         throw err;
       });
     })
   });
-  describe('rES 10 items, trying a negative iterable index', ()=>{
-    it('should throw an error', done => {
-      let items = [1,2,3,4,5,6,7,8,9,10];
-      let init = Date.now();
-      rES(items, fastrESError, 5, -1).then(()=>{
-        let end = Date.now();
-        chai.assert.equal(Math.floor((end - init)/1000),2);
-        done();
-      }).catch(err => {
-        console.log(err);
-        done();
-      });
-    })
-  });
-  describe('rSubSet 10 items by subsets of 3 items each. Each item 3 seconds, with no errors', ()=>{
+  describe('rSubSeq 10 items by subsets of 3 items each. Each item 3 seconds, with no errors', ()=>{
     it('should finish all processes in 12 seconds', done => {
       let items = [1,2,3,4,5,6,7,8,9,10];
       let init = Date.now();
-      rSubSet(items, processItemNoError, 3).then(()=>{
+      rSubSeq(items, processItemNoError, 3).then(()=>{
         let end = Date.now();
         chai.assert.equal(Math.floor((end - init)/1000),12);
         done();
@@ -90,75 +76,16 @@ describe('Promises resolve flow control', ()=>{
       });
     })
   });
-  describe('rSubSet 10 items by subsets of 3 items each. Each item 3 seconds, only pair items are valid, the others give error', ()=>{
+  describe('rSubSeq 10 items by subsets of 3 items each. Each item 3 seconds, only pair items are valid, the others give error', ()=>{
     it('should finish all processes in 12 seconds because in each subset there is at least 1 valid item which takes 3 seconds to be processed', done => {
       let items = [1,2,3,4,5,6,7,8,9,10];
       let init = Date.now();
-      rSubSet(items, processItemError, 3).then(()=>{
+      rSubSeq(items, processItemError, 3).then(()=>{
         let end = Date.now();
         chai.assert.equal(Math.floor((end - init)/1000),12);
         done();
       }).catch(err => {
         throw err;
-      });
-    })
-  });
-  describe('rSubSet 10 items by subsets of 3 items each. Each item 3 seconds, initial subset out of range', ()=>{
-    it('should not process any item, taking whole process 0 seconds', done => {
-      let items = [1,2,3,4,5,6,7,8,9,10];
-      let init = Date.now();
-      rSubSet(items, processItemNoError, 3, 5).then(()=>{
-        let end = Date.now();
-        chai.assert.equal(Math.floor((end - init)/1000),0);
-        done();
-      }).catch(err => {
-        throw err;
-      });
-    })
-  });
-  describe('rSubSet 10 items by subsets of 3 items each. Each item 3 seconds, initial subset is the last one', ()=>{
-    it('should process just item 10, taking whole process 3 seconds', done => {
-      let items = [1,2,3,4,5,6,7,8,9,10];
-      let init = Date.now();
-      rSubSet(items, processItemNoError, 3, 3).then(()=>{
-        let end = Date.now();
-        chai.assert.equal(Math.floor((end - init)/1000),3);
-        done();
-      }).catch(err => {
-        throw err;
-      });
-    })
-  });
-  describe('rSubSet 10 items by subsets of 13 items each. Each item 3 seconds', ()=>{
-    it('should process all items, taking whole process 3 seconds', done => {
-      let items = [1,2,3,4,5,6,7,8,9,10];
-      let init = Date.now();
-      rSubSet(items, processItemNoError, 13).then(()=>{
-        let end = Date.now();
-        chai.assert.equal(Math.floor((end - init)/1000),3);
-        done();
-      }).catch(err => {
-        throw err;
-      });
-    })
-  });
-  describe('rSubSet with negative index', ()=>{
-    it('should throw an error and do not process anything', done => {
-      let items = [1,2,3,4,5,6,7,8,9,10];
-      rSubSet(items, processItemNoError, 13, -3).then(()=>{
-      }).catch(err => {
-        console.log(err);
-        done();
-      });
-    })
-  });
-  describe('rSubSet with negative limit', ()=>{
-    it('should throw an error and do not process anything', done => {
-      let items = [1,2,3,4,5,6,7,8,9,10];
-      rSubSet(items, processItemNoError, -2).then(()=>{
-      }).catch(err => {
-        console.log(err);
-        done();
       });
     })
   });
@@ -166,7 +93,7 @@ describe('Promises resolve flow control', ()=>{
     it('rSeq will take 9 seconds', done => {
       let items = [1,2,3];
       let init = Date.now();
-      rSeq(items, processItemNoError).then(()=>{
+      rSeq(processItemNoError, items).then(()=>{
         let end = Date.now();
         chai.assert.equal(Math.floor((end - init)/1000),9);
         done();
@@ -176,7 +103,7 @@ describe('Promises resolve flow control', ()=>{
       });
     });
   });
-  describe('rSubSet 2 by 2', ()=>{
+  describe('rSubSeq 2 by 2', ()=>{
     it('rSubSeq1 should take 6 seconds', done => {
       let items = [1,2,3];
       let init = Date.now();
